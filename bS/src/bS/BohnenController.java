60810d8b5905573d94c960a22caf8cb35e988f45
Minh Huy Nguyen <minh_huy.nguyen@stud.htwk-leipzig.de>
2024-08-15 23:11:45 +0200

    first controller

---
+++ bS/src/bS/BohnenController.java
@@ -0,0 +1,76 @@
+package bS;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import bS.BohnenView.*;
+
+
+public class BohnenController implements ActionListener {
+    private BohnenModel model;
+	private TitleScreen view;
+    private NameInput nameInput;
+
+	protected BohnenController(BohnenModel model, TitleScreen view) {
+		this.model = model;
+		this.view = view;
+
+		view.titleActionListener(this);
+	}
+
+    public void actionPerformed(ActionEvent evt) {
+        String actionEvent = evt.getActionCommand();
+        switch (actionEvent) {
+            case "startBtn":{
+                nameInput = new NameInput();
+                NameInputController nameInputController = new NameInputController(model, nameInput, false);
+                nameInput.nameActionListener(nameInputController); 
+                nameInput.setVisible(true);
+
+                break;                
+            }
+            case "debugBtn":{
+                nameInput = new NameInput();
+                NameInputController nameInputController = new NameInputController(model, nameInput, true);
+                nameInput.nameActionListener(nameInputController); 
+                nameInput.setVisible(true);
+                break;                         
+            }
+            case "quitBtn":{
+                view.dispose();
+                break;                
+            }
+
+            default:
+                break;
+        }
+    }
+
+}
+
+class NameInputController implements ActionListener {
+	private BohnenModel model;
+	private NameInput view;
+    protected boolean isDebug;
+
+
+    protected  NameInputController(BohnenModel model, NameInput view, boolean isDebug){
+        this.model = model;
+        this.view = view;
+        this.isDebug = isDebug;
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        String actionEvent = evt.getActionCommand();
+        switch (actionEvent) {
+            case "okBtn": {
+                model.startGame(view.getPlayerName(), isDebug);
+                model.printBoard();
+                break;
+            }
+            case "clrBtn": {
+                view.dispose();
+                break;
+            }
+        }
+    }
+}
